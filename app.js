d07f9f32eb491a30614eb5ffcd323379a03b3653
Salem Eid <salem.eid@gmail.com>
2019-11-05 08:09:14 +0300

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 
 const app = express()
+var items = ["hello","world","food"];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -14,69 +15,23 @@ app.set("view engine", "ejs");
 app.get("/",function(req, res){
 
   var today = new Date();
-  var day = "";
-  var msg = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (today.getDay()) {
-    case 6:
-    day = "Saturday";
-    msg = "Hooray!";
-    break;
-    case 0:
-    day = "Sunday";
-    msg = "Hooray!";
-    break;
-    case 1:
-    day = "Monday";
-    msg = "Booooooo!";
-    break;
-    case 2:
-    day = "Tuesday";
-    msg = "Booooooo!";
-    break;
-    case 3:
-    day = "Wednesday";
-    msg = "Booooooo!";
-    break;
-    case 4:
-    day = "Thursday";
-    msg = "Booooooo!";
-    break;
-    case 5:
-    day = "Friday";
-    msg = "Booooooo!";
-      break;
-    default:
-    app.get("system error");
+  var day = today.toLocaleDateString("en-US",options);
 
-  }
-  //
-  // //**if (today.getDay() === 6) {
-  //   day = "Saturday";
-  //   msg = "Hooray!";
-  // } else if (today.getDay() === 0){
-  //   day = "Sunday";
-  //   msg = "Hooray!";
-  // } else if (today.getDay() === 1){
-  //   day = "Monday";
-  //   msg = "Boooo!";
-  // } else if (today.getDay() === 2){
-  //   day = "Tuesday";
-  //   msg = "Boooo!";
-  // } else if (today.getDay() === 3){
-  //   day = "Wednsday";
-  //   msg = "Boooo!";
-  // } else if (today.getDay() === 4){
-  //   day = "Thursday";
-  //   msg = "Boooo!";
-  // } else if (today.getDay() === 5){
-  //   day = "Friday";
-  //   msg = "Boooo!";
-  // }
-
-  res.render("list", {kindOfDay: day, message: msg});
+  res.render("list", {kindOfDay: day, newListItems: items});
 });
 
+
+app.post("/",function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000,function(){
   console.log("Running");
